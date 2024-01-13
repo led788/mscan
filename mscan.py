3c285d4351ad00a571f477a40f7d001c613d6ce6
led <led@led-mini.local>
2024-01-13 21:23:05 +0300

    add mode 2 for discounted prices

--- mscan.py
+++ mscan.py
@@ -7,7 +7,7 @@ from selenium.common.exceptions import NoSuchElementException
 import click
 
 
-def look_high_bonuses(driver, percent):
+def look_high_bonuses(driver, percent, mode):
     items = driver.find_elements(By.CSS_SELECTOR, 'div.item-block')
 
     print(f'found {len(items)} items')
@@ -15,28 +15,46 @@ def look_high_bonuses(driver, percent):
     if len(items) > 0:
         for item in items:
 
-            try:
-                bonus = item.find_element(By.CLASS_NAME, 'bonus-percent').text
-                bonus = int(re.sub(r'\D', '', bonus))
+            if mode == 1:
+                try:
+                    bonus = item.find_element(By.CLASS_NAME, 'bonus-percent').text
+                    bonus = int(re.sub(r'\D', '', bonus))
 
-                if bonus > percent:
-                    price = item.find_element(By.CLASS_NAME, 'item-price').text
-                    price = int(re.sub(r'\D', '', price))
-                    title = item.find_element(By.CLASS_NAME, 'ddl_product_link').text
-                    link = item.find_element(By.CLASS_NAME, 'ddl_product_link').get_attribute('href')
+                    if bonus >= percent:
+                        price = item.find_element(By.CLASS_NAME, 'item-price').text
+                        price = int(re.sub(r'\D', '', price))
+                        title = item.find_element(By.CLASS_NAME, 'ddl_product_link').text
+                        link = item.find_element(By.CLASS_NAME, 'ddl_product_link').get_attribute('href')
 
-                    print(f'{title=}')
-                    print(f'{link=}')
-                    print(f'{price=} {bonus=}')
-            except NoSuchElementException:
-                continue
+                        print(f'{title=}')
+                        print(f'{link=}')
+                        print(f'{price=} {bonus=}')
+                except NoSuchElementException:
+                    continue
+            elif mode == 2:
+                try:
+                    discount = item.find_element(By.CLASS_NAME, 'discount-percentage__value').text
+                    discount = int(re.sub(r'\D', '', discount))
+
+                    if discount >= percent:
+                        price = item.find_element(By.CLASS_NAME, 'item-price').text
+                        price = int(re.sub(r'\D', '', price))
+                        title = item.find_element(By.CLASS_NAME, 'ddl_product_link').text
+                        link = item.find_element(By.CLASS_NAME, 'ddl_product_link').get_attribute('href')
+
+                        print(f'{title=}')
+                        print(f'{link=}')
+                        print(f'{price=} {discount=}')
+                except NoSuchElementException:
+                    continue
 
 
 @click.command()
 @click.option('-n', '--pages', type=click.INT, required=True, help='Start megamarket category URL.')
 @click.option('-p', '--percent', type=click.INT, required=True, help="A positive percent number.")
+@click.option('-m', '--mode', type=click.INT, default=1, help="Mode 1 look bonus, mode 2 look discount")
 @click.argument('start_url', type=click.STRING, required=True)
-def mscan(start_url, percent, pages):
+def mscan(start_url, percent, pages, mode):
     '''
 
     :param start_url:
@@ -50,15 +68,15 @@ def mscan(start_url, percent, pages):
     print(f'*** page 1')
     driver.get(start_url)
     time.sleep(2)
-    look_high_bonuses(driver, percent)
+    look_high_bonuses(driver, percent, mode)
 
     for page_num in range(2, pages + 1):
         print(f'*** page {page_num}')
         driver.get(start_url + 'page-' + str(page_num) + '/')
         time.sleep(2)
-        look_high_bonuses(driver, percent)
+        look_high_bonuses(driver, percent, mode)
 
-    driver.close()
+    driver.quit()
 
 
 if __name__ == '__main__':
